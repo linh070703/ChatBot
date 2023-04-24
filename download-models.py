0de7577b9eb2e1049e04a8058db9ac9cd0093983
thaiminhpv <thaiminhpv@gmail.com>
2023-04-24 18:14:00 +0700

    first work model

--- download-models.py
+++ download-models.py
@@ -1,32 +1,17 @@
-import os
-os.environ['HUGGINGFACE_HUB_CACHE'] = '/media/saplab/Data_Win/jh/hf_home/'
+import sys
+import tempfile
 
-from huggingface_hub import snapshot_download
-from tqdm import tqdm
+import huggingface_hub
 
-import huggingface_hub 
-huggingface_hub.__version__
+if len(sys.argv) < 3:
+    sys.exit("usage: python download.py REPO_ID LOCAL_DIR [REVISION]")
 
-def downloads(name:str):
-#     ! mkdir -p "/media/saplab/Data_Win/jh/models/$name"
-    snapshot_download(
-        repo_id=name,
-        ignore_patterns="*.pt",
-#         local_dir="/media/saplab/Data_Win/jh/models/" + name,
-#         local_dir_use_symlinks=False
+with tempfile.TemporaryDirectory() as cache_dir:
+    huggingface_hub.snapshot_download(
+        repo_id=sys.argv[1],
+        local_dir=sys.argv[2],
+        revision=sys.argv[3] if len(sys.argv) > 3 else None,
+        cache_dir=cache_dir,
+        local_dir_use_symlinks=False,
+        resume_download=True,
     )
-
-# downloads("NlpHUST/gpt-neo-vi-small")
-# downloads("NlpHUST/gpt2-vietnamese")
-# downloads("VietAI/gpt-neo-1.3B-vietnamese-news")
-# downloads("VietAI/gpt-j-6B-vietnamese-news")
-
-# downloads("bigscience/mt0-xxl-mt")
-downloads("bigscience/mt0-small")
-downloads("bigscience/mt0-base")
-downloads("bigscience/mt0-large")
-# downloads("bigscience/mt0-xl")
-downloads("bigscience/bloomz-560m")
-downloads("bigscience/bloomz-1b1")
-downloads("bigscience/bloomz-1b7")
-downloads("bigscience/bloomz-3b")
