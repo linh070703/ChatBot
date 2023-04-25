47f11d415300e9c39132c5eeebd0f4a098c51058
thaiminhpv <thaiminhpv@gmail.com>
2023-04-25 21:33:00 +0700

    create mock for /chat

--- app.py
+++ app.py
@@ -77,6 +77,47 @@ def chat():
         'action': action
     })
 
+@app.route('/mock/chat', methods=['POST'])
+def chat():
+    _ = request.json.get('model', 'gpt4all')
+    temperature = request.json.get('temperature', 0.7)
+    messages = request.json.get('messages', [])
+    n_predict = request.json.get('max_num_words', 64)
+    stream = request.json.get('stream', False)
+    stream_json = request.json.get('stream_json', False)
+    userInfo = request.json.get('userInfo', {"status": None})
+    if stream:
+        return jsonify({'error': 'Stream mode is not supported.'})
+    if not messages:
+        return jsonify({'error': 'No messages provided.'})
+    print(f"Received request: {request.json}")
+
+    # Get chat answer
+    conversation = Conversation({'command': MODE['chat'], 'messages': messages, 'userInfo': userInfo})
+    input = conversation.raw_conversation
+    print(f"Input: {input}")
+
+    assistant_answer = "Sure Onii-chan 🥺! I will send Minh 100$ for the pizza."
+    
+    print(f"Assistant_answer: {assistant_answer}")
+
+    conversation.command = MODE['action']
+
+    next_input = conversation.prepare_model_input() + assistant_answer
+
+    print(f"Next_input: {next_input}")
+
+    raw_action = "Action:TRANSFER_MONEY[amount=100, from=Alex, to=Minh]"
+
+    action = conversation.extract_action(raw_action)
+
+    return jsonify({
+        'message': {
+            'role': 'assistant', 'content': assistant_answer
+        },
+        'action': action
+    })
+
 @app.route('/api/detect-intent', methods=['POST'])
 def chat():
     _ = request.json.get('model', 'gpt4all')
