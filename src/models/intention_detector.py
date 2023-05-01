ed33bbb154839f3bcf97372e414552a9bbf8b786
thaiminhpv <thaiminhpv@gmail.com>
2023-05-01 21:16:06 +0700

    typo

--- src/models/intention_detector.py
+++ src/models/intention_detector.py
@@ -4,7 +4,8 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from typing import List, Literal, Dict, Union, Any, Tuple
 from utils.model_api import generate_action_chatgpt_api
-from utils.logger import setup_logging_display_only, print
+from utils.logger import setup_logging_display_only
+import logging
 
 PROMPT = """This is a user's intention detecting system. This system is able to detect intention of users in conversation history and direct message. English and Vietnamese are supported. There are 6 possible user's intentions: CHECK_BALANCE, TRANSFER, TRANSFER_TO_EACH_USERS, CREATE_CHAT_GROUP, ASK_ASSISTANT, NO_SYSTEM_ACTION."""
 # Minh: Tao muốn chuyển khoản cho Nam 300k.
@@ -34,7 +35,7 @@ def dectect_user_intention(
     conversation = "\n".join([f"{message['user']}: {message['message']}" for message in messages])
     last_user = messages[-1]['user']
     model_input = f"{PROMPT}\n{conversation}\n{last_user}'s intention: "
-    print("Model input: \n", model_input)
+    logging.info(f"Model input: \n{model_input}")
     output = generate_action_chatgpt_api(model_input)
     intent = output.strip()
     assert intent in ["CHECK_BALANCE", "TRANSFER", "TRANSFER_TO_EACH_USERS", "CREATE_CHAT_GROUP", "ASK_ASSISTANT", "NO_SYSTEM_ACTION"], f"Invalid intent: {intent}"
@@ -44,11 +45,7 @@ def dectect_user_intention(
 if __name__ == "__main__":
     setup_logging_display_only()
     out = dectect_user_intention(messages=[
-        {"user": "Minh", "message": "Hello, I want to check my account balance"},
-        {"user": "Lan", "message": "Ready for a party?"},
-        {"user": "Minh", "message": "Yes, I am ready."},
-        {"user": "Hung", "message": "I want to create a chat group with Minh and Lan."},
-        {"user": "Cường", "message": "Gửi cho mỗi thằng 600tr"},
+        {"user": "Cường", "message": "Tôi muốn được tư vấn về việc trả nợ."},
     ])
-    print("Output: ", out)
+    logging.info(f"Output: {out}")
         
