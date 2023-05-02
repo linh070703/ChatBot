65d501391def513099359e3000eef4e66bc3ec8f
thaiminhpv <thaiminhpv@gmail.com>
2023-05-02 17:27:08 +0700

    detect if is_money_management_question

--- src/expert_system/money_management.py
+++ src/expert_system/money_management.py
@@ -1,7 +1,26 @@
 from typing import Dict, List, Tuple, Union, Literal, Any
 
 def is_money_management_question(message: str) -> bool:
-    ...
+    """
+    Lên kế hoạch tiết kiệm.
+
+    Args:
+        message (str): Message from user.
+    
+    Returns:
+        bool: True if the message is about money management, False otherwise.
+    
+    Example:
+    >>> is_money_management_question("Tôi muốn lên kế hoạch tiết kiệm.")
+    True
+    >>> is_money_management_question("Tính toán kế hoạch tiết kiệm mục tiêu của bạn.")
+    True
+    """
+
+    return (
+        "kế hoạch tiết kiệm" in message 
+    )
+
 
 def money_management_suggestion(messages: List[Dict[str, str]]) -> Tuple[str, List[str]]:
     ...
