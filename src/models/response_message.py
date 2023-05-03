f1f2cc79957d67df21f89f6e98a317a80a8994a9
thaiminhpv <thaiminhpv@gmail.com>
2023-05-03 09:36:29 +0000

    update response_message

--- src/models/response_message.py
+++ src/models/response_message.py
@@ -8,8 +8,11 @@ def get_response_message(
     Get response message from assistant.
     """
     if action == 'TRANSFER':
-        return f"Chuyển {params['amount']} cho {params['receiver']} thành công."
+        return f"Đã chuyển {params['amount']} cho {params['receiver']} với nội dung \"{params['msg']}\"."
     elif action == 'TRANSFER_TO_EACH_USERS':
-        return f"Đã chuyển cho mỗi người {params['amount_each']} thành công."
+        return f"Đã chuyển cho mỗi người trong nhóm {params['amount_each']} với nội dung \"{params['msg']}\"."
     elif action == 'CREATE_CHAT_GROUP':
-        return f"Tạo nhóm chat {params['group_name']} thành công."
+        if params['group_name']:
+            return f"Đã tạo nhóm chat {params['group_name']} thành công."
+        else:
+            return f"Đã tạo nhóm chat thành công."
