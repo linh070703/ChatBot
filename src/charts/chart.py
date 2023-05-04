33d3b92a30a7169d558d2fa2f5821457eb02f49e
DAN3002 <dinhanh300229@gmail.com>
2023-05-04 10:58:28 +0700

    Init compare chart

--- src/charts/chart.py
+++ src/charts/chart.py
@@ -1,10 +1,21 @@
 from flask import (
-    Blueprint,
-    render_template
+	Blueprint,
+	render_template,
+	request
 )
+
+from src.charts.utils.get_metrics import get_metrics
 
 chart = Blueprint('chart', __name__)
 
-@chart.route('/compare')
+@chart.route('/compare', methods=['GET'])
 def compare():
-    return render_template('iframe.html')
+	symbols = request.args.get('symbols').split(',')
+	
+	metrics = {}
+	for symbol in symbols:
+		metrics[symbol] = get_metrics(symbol)
+
+	print(metrics)
+
+	return render_template('iframe.html')
