8835f8a7029427cd0ae80fcdbf717762b1e1f699
thaiminhpv <thaiminhpv@gmail.com>
2023-05-06 11:39:02 +0700

    Optimize Prompt engineering: move checkbox to end of line after reason for SIGNIFICANT improvement

--- src/models/action.py
+++ src/models/action.py
@@ -59,10 +59,10 @@ def get_action_params(
         conversation = "\n".join([f"{' '.join(message['user'].split())}: {' '.join(message['content'].split())}" for message in messages])
         last_user = messages[-1]['user']
         model_input = f"""This is a financial assistant system that can TRANSFER money when user request. English and Vietnamese are supported. System's action syntax is: TRANSFER[<receiver>,<amount>|<message>]. Note that money abbreviation should be expanded without comma or dot. E.g. (30k=30000, 24tr=24000000, 5 nghìn=5000, tám chục nghìn=80000).
-System will first output "REASONING: <thinking about user's intention, between 3 to 5 sentences, and analyze each param 'receiver', 'amount', and 'message'>". Then, system will output "CHECKLIST: " and then tick a checklist (using [x]) for each param:
-[ ] receiver (need to be real username)
-[ ] amount (need to be number)
-[ ] message (Is the purpose of the transaction specified?)
+System will first output "REASONING: <thinking about user's intention, between 3 to 5 sentences, and analyze each param 'receiver', 'amount', and 'message'>". Then, system will output "CHECKLIST: " and then including reason with a tick to the checklist for each param:
+receiver (need to be real username) [ ]
+amount (need to be number) [ ]
+message (Is the purpose of the transaction specified?) [ ]
 Then, if any of the above param is missing, e.g. user did not explicitly mention receiver's username or message, then system will follows with "RESULT: NOT_ENOUGH_PARAMS". "ENOUGH_PARAMS" otherwise.
 If RESULT is NOT_ENOUGH_PARAMS, then system will output a response "RESPONSE: ..." asking user to provide more information. Note that system should response in the same language as User's question.
 If RESULT is ENOUGH_PARAMS, then system will output the system action "ACTION: TRANSFER[...]"
@@ -74,7 +74,7 @@ REASONING:"""
         output = generate_general_call_chatgpt_api(
             inputs=model_input,
             temperature=0,
-            max_tokens=256,
+            max_tokens=512,
         )
         logging.info(f"Model output: \n{output}")
 
@@ -98,9 +98,9 @@ REASONING:"""
     elif action == "CREATE_CHAT_GROUP":
         conversation = "\n".join([f"{' '.join(message['user'].split())}: {' '.join(message['content'].split())}" for message in messages])
         last_user = messages[-1]['user']
-        model_input = f"""This is an assistant system that can CREATE_CHAT_GROUP when user request. English and Vietnamese are supported. System's action syntax is: CREATE_CHAT_GROUP[<user_comma_separated>|<group_name (nullable)>]. System will first output "REASONING: <thinking about user's intention, between 30-50 words, and analyze each param 'user_comma_separated' and 'group_name'>". Then, system will output "CHECKLIST: " and then tick a checklist (using [x]) for each param:
-[ ] user_comma_separated (need to be a list of username with comma separated)
-[ ] group_name (Did user explicitly mention the group name?)
+        model_input = f"""This is an assistant system that can CREATE_CHAT_GROUP when user request. English and Vietnamese are supported. System's action syntax is: CREATE_CHAT_GROUP[<user_comma_separated>|<group_name (nullable)>]. System will first output "REASONING: <thinking about user's intention, between 30-50 words, and analyze each param 'user_comma_separated' and 'group_name'>". Then, system will output "CHECKLIST: " and then including reason with a tick to the checklist for each param: 
+user_comma_separated (need to be a list of username with comma separated) [ ]
+group_name (Did user explicitly mention the group name?) [ ]
 Then, if user did not explicitly mention the list of users, then system will follows with "RESULT: NO_USERS". "OK" otherwise.
 If RESULT is NO_USERS, then system will output a response "RESPONSE: ..." asking user to provide user list. Note that system should response in the same language as User's question.
 If RESULT is OK, then system will output the system action "ACTION: CREATE_CHAT_GROUP[...]"
@@ -136,9 +136,9 @@ REASONING:"""
         conversation = "\n".join([f"{' '.join(message['user'].split())}: {' '.join(message['content'].split())}" for message in messages])
         last_user = messages[-1]['user']
         model_input = f"""This is a financial assistant system that can TRANSFER_TO_EACH_USERS money when user request. English and Vietnamese are supported. System's action syntax is: TRANSFER_TO_EACH_USERS[<amount>|<message>]. Note that money abbreviation should be expanded without comma or dot. E.g. (30k=30000, 24tr=24000000, 5 nghìn=5000, tám chục nghìn=80000).
-System will first output "REASONING: <thinking about user's intention, between 30-50 words, and analyze each param 'amount' and 'message'>". Then, system will output "CHECKLIST: " and then tick a checklist (using [x]) for each param:
-[ ] amount (need to be number)
-[ ] message (Is the purpose of the transaction specified?)
+System will first output "REASONING: <thinking about user's intention, between 30-50 words, and analyze each param 'amount' and 'message'>". Then, system will output "CHECKLIST: " and then including reason with a tick to the checklist for each param:
+amount (need to be number) [ ]
+message (Is the purpose of the transaction specified?) [ ]
 Then, if any of the param is missing, e.g. user did not explicitly mention the transaction purpose, then system will follows with "RESULT: NOT_ENOUGH_PARAMS". "ENOUGH_PARAMS" otherwise.
 If RESULT is NOT_ENOUGH_PARAMS, then system will output a response "RESPONSE: ..." asking user to provide more information. Note that system should response in the same language as User's question.
 If RESULT is ENOUGH_PARAMS, then system will output the system action "ACTION: TRANSFER_TO_EACH_USERS[...]"
@@ -174,13 +174,13 @@ if __name__ == "__main__":
     setup_logging_display_only()
     out = get_action_params(messages=[
         {"user": "Minh", "content": "Hello, I want to check my account balance"},
-        {"user": "Lan", "content": "Ready for a party?"},
-        {"user": "Minh", "content": "Yes, I am ready."},
-        {"user": "Minh", "content": "I want to transfer 3289723 VND to Hung."},
-        {"user": "Cường", "content": "Chuyển quà sinh nhật mỗi cháu 400k"},
-        {"user": "Minh", "content": "Chuyển Alisa ba chục nghìn tiền bún đậu"},
-        {"user": "Minh", "content": "I want to transfer 3289723 VND to Hung với lời nhắn là 'Chúc mừng sinh nhật nhé, nhớ học giỏi'."},
-        {"user": "Hung", "content": "I want to create a chat group for Minh, me, Lan, and Cường named 'Party| this sunday'."},
+        # {"user": "Lan", "content": "Ready for a party?"},
+        # {"user": "Minh", "content": "Yes, I am ready."},
+        # {"user": "Minh", "content": "I want to transfer 3289723 VND to Hung."},
+        # {"user": "Cường", "content": "Chuyển quà sinh nhật mỗi cháu 400k"},
+        # {"user": "Minh", "content": "Chuyển Alisa ba chục nghìn tiền bún đậu"},
+        # {"user": "Minh", "content": "I want to transfer 3289723 VND to Hung với lời nhắn là 'Chúc mừng sinh nhật nhé, nhớ học giỏi'."},
+        {"user": "Hung", "content": "I want to create a chat group for Minh, me, Lan, and Cường for this birthday."},
     ], action="CREATE_CHAT_GROUP")
     print("Output: ", out)
         
