ddcbd478231949d9ea53b627c4037b816b104dec
DAN3002 <dinhanh300229@gmail.com>
2023-05-06 10:26:38 +0700

    Add earning-line

--- src/charts/chart.py
+++ src/charts/chart.py
@@ -24,17 +24,10 @@ def compare():
 
 @chart.route('/earning-bar', methods=['GET'])
 def earning_bar():
+    LIMIT = 8
     symbol = request.args.get('symbol')
 
     earning_data = get_earning_data(symbol)
-
-    # map to each element become reportedEPS - estimatedEPS
-    # data = list(map(lambda x: float(
-    #     x['reportedEPS']) - float(x['estimatedEPS']), earning_data))
-    
-    # # Round to 2 decimal places
-    # data = list(map(lambda x: round(x, 2), data))
-    LIMIT = 8
     earning_data = earning_data[:min(LIMIT, len(earning_data))]
 
 
@@ -43,3 +36,16 @@ def earning_bar():
     estimatedEPS = list(map(lambda x: round(float(x['estimatedEPS']), 2), earning_data))
 
     return render_template('earning-bar.html', reportedEPS=reportedEPS, estimatedEPS=estimatedEPS, labels=labels, symbol=symbol)
+
+@chart.route('/earning-line', methods=['GET'])
+def earning_line():
+    LIMIT = 16
+    symbol = request.args.get('symbol')
+
+    earning_data = get_earning_data(symbol)
+    earning_data = earning_data[:min(LIMIT, len(earning_data))]
+
+    labels = list(map(lambda x: x['fiscalDateEnding'], earning_data))
+    reportedEPS = list(map(lambda x: round(float(x['reportedEPS']), 2), earning_data))
+
+    return render_template('earning-line.html', reportedEPS=reportedEPS, labels=labels, symbol=symbol)
