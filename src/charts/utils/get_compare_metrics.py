1fa901b7b744edd217effb43fdc3818b56b6f22e
DAN3002 <dinhanh300229@gmail.com>
2023-05-06 11:03:23 +0700

    Get metric for compare chart

--- src/charts/utils/get_compare_metrics.py
+++ src/charts/utils/get_compare_metrics.py
@@ -1,26 +1,32 @@
-import os
-from alpha_vantage.timeseries import TimeSeries
+import requests
+from bs4 import BeautifulSoup
 
 def get_compare_metrics(symbol):
-	ts = TimeSeries(
-		key=os.environ.get('ALPHA_VANTAGE_API_KEY'),
-		output_format='pandas'
-	)
-	data, meta_data = ts.get_intraday(symbol=symbol)
+	endpoint = f'https://volafy.net/equity/{symbol}'
 
-	volatility = (data['2. high'] - data['3. low']) / data['2. high']
+	try:
+		# Get HTML and parse it
+		response = requests.get(endpoint)
 
-	returns = data['4. close'].pct_change()
+		soup = BeautifulSoup(response.text, 'html.parser')
 
-	initial_price = data.iloc[0]['4. close']
-	current_price = data.iloc[-1]['4. close']
-	roi = (current_price - initial_price) / initial_price * 100
+		# Get metrics
+		elements_volatility = soup.find_all('span', class_='volatility-card_colItemValue__9Usfd')
+		volatility = elements_volatility[3].text
 
-	print(returns)
-	
+		elements_return = soup.find_all('div', class_='symbol-title-price_priceWrapper__5jyjK')[0]
+		# get text of last child
+		return_value = elements_return.contents[-1].text
 
-	return {
-		'volatility': volatility.mean(),
-		'return': returns[0],
-		'roi': roi
-	}
+		# remove % sign and parse to float
+		volatility = float(volatility[:-1])
+		return_value = float(return_value.split('%')[0])
+
+		return {
+			'volatility': volatility,
+			'return': return_value
+		}
+	except:
+		print('Error')
+		return None
+
